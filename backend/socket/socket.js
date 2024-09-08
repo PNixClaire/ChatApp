30581ddb8566114ce4355e763cbc09a4a860ba5a
PNixClaire <phoenix_decastro@dlsu.edu.ph>
2024-09-08 11:13:00 +0800

    edit

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,11 +9,11 @@ const server = http.createServer(app);
 
 //socket io server
 const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:5000"],
-        methods: ["GET", "POST"]
-    }
-})
+  cors: {
+    origin: ["http://localhost:3000", "https://chatapp-c5m6.onrender.com/"],
+    methods: ["GET", "POST"],
+  },
+});
 
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
